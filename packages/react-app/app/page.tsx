5e4b3acb4b1cb4dbfbcd15514273506f6f1db7f0
Kris Haamer <kris@haam.co>
2024-11-16 14:21:31 +0700

    Switch to Alfajores

--- packages/react-app/app/page.tsx
+++ packages/react-app/app/page.tsx
@@ -4,49 +4,10 @@ import { useWeb3 } from "@/contexts/useWeb3";
 import { NotificationForm } from "../components/Push";
 import { useEffect, useState } from "react";
 
-const switchToAlfajores = async () => {
-  try {
-    await window.ethereum.request({
-      method: "wallet_switchEthereumChain",
-      params: [{ chainId: "0xaef3" }], // Hexadecimal for 44787
-    });
-  } catch (switchError) {
-    // Assert the type of switchError
-    const error = switchError as any;
-
-    console.error("Network switch failed:", error);
-
-    if (error.code === 4902) {
-      // Chain not added to wallet
-      try {
-        await window.ethereum.request({
-          method: "wallet_addEthereumChain",
-          params: [
-            {
-              chainId: "0xaef3",
-              chainName: "Celo Alfajores Testnet",
-              rpcUrls: ["https://alfajores-forno.celo-testnet.org"],
-              nativeCurrency: {
-                name: "Celo",
-                symbol: "CELO",
-                decimals: 18,
-              },
-              blockExplorerUrls: ["https://alfajores.celoscan.io"],
-            },
-          ],
-        });
-      } catch (addError) {
-        console.error("Add network failed:", addError);
-      }
-    }
-  }
-};
-
 export default function Home() {
   const { address, getUserAddress } = useWeb3();
 
   useEffect(() => {
-    switchToAlfajores();
     getUserAddress();
   }, []);
 
