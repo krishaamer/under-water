33ec7f7d85316b99fb9ce978904dda42cb0574ed
Kris Haamer <kris@haam.co>
2024-11-17 06:55:29 +0700

    Update UI

--- packages/react-app/app/game/page.tsx
+++ packages/react-app/app/game/page.tsx
@@ -81,7 +81,6 @@ export default function Home() {
         {address && (
           <>
             <div className="foreground">
-              <OffsetCelo />
               <Apples onAppleClick={handleAppleClick} />
               {allDistrictsClicked ? (
                 <>
